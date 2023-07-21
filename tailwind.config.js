fd3fd199352f8625df58b2679734a1672002a0d7
Prechyious <lemoprechy@gmail.com>
2023-07-21 11:32:18 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                "dark-mode-elements": "hsl(209, 23%, 22%)",
+                "dark-mode-bg": "hsl(207, 26%, 17%)",
+                "light-mode-text": "hsl(200, 15%, 8%)",
+                "light-mode-input": "hsl(0, 0%, 52%)",
+                "light-mode-bg": "hsl(0, 0%, 98%)",
+                "light-mode-lements": "hsl(0, 0%, 100%)",
+            },
+        },
+    },
+    plugins: [],
+};

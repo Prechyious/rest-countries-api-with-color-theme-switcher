94af90a21ba86b3a4159f6d9fbe592505e8d4f36
Prechyious <lemoprechy@gmail.com>
2023-07-26 10:47:59 +0100

    Fiexd Dark Mode

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,11 @@ import { FaMoon, FaSun } from "react-icons/fa6";
 const NavBar = () => {
     const [darkMode, setDarkMode] = useState(false);
 
-    const changeTheme = () => {
-        document.body.classList.toggle("dark");
-        setDarkMode(!darkMode);
+    // Function to toggle dark mode
+    const toggleDarkMode = () => {
+        const newDarkMode = !darkMode;
+        setDarkMode(newDarkMode);
+        localStorage.setItem("darkMode", JSON.stringify(newDarkMode));
     };
 
     useEffect(() => {
@@ -15,16 +17,24 @@ const NavBar = () => {
             "(prefers-color-scheme: dark)"
         ).matches;
 
-        // Set the initial theme based on the user's preference
-        setDarkMode(prefersDarkMode);
+        // Get the user's preference from local storage (if available)
+        const storedDarkMode = JSON.parse(localStorage.getItem("darkMode"));
+
+        // Set the initial theme based on the user's preference (if available)
+        setDarkMode(storedDarkMode ?? prefersDarkMode);
 
         // Add or remove the 'dark' class to the body based on the user's preference
-        if (prefersDarkMode) {
-            document.body.classList.add("dark");
+        if (storedDarkMode !== null) {
+            document.body.classList.toggle("dark", storedDarkMode);
         } else {
-            document.body.classList.remove("dark");
+            document.body.classList.toggle("dark", prefersDarkMode);
         }
     }, []);
+
+    useEffect(() => {
+        // Update the body class whenever darkMode changes
+        document.body.classList.toggle("dark", darkMode);
+    }, [darkMode]);
 
     return (
         <nav className="sticky top-0 left-0 flex items-center justify-between p-5 md:py-5 md:px-10 shadow-md z-10 bg-light-mode-bg dark:bg-dark-mode-bg dark:text-light-mode-lements">
